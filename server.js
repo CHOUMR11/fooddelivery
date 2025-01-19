1f8a853164681d23bdc0990b6db2c8e1ad981116
Oussama <chammaroussama@gmail.com>
2025-01-19 16:36:45 +0100

    onessss commit

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ import userRouter from "./routes/userRoute.js";
 import "dotenv/config.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from "path"; // Add this line to import the 'path' module
+
+// Utilisation de import.meta.url pour obtenir __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // app config
 const app = express();
+
+// port config
 const port = process.env.PORT || 4000;
 
 // middleware
@@ -23,12 +32,15 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
-app.use("/api/order",orderRouter)
+app.use("/api/order", orderRouter);
 
-app.get("/", (req, res) => {
-  res.send("API Working");
+// dist reactjs
+app.use(express.static(path.join(__dirname, './client/build'))); // Route pour les pages non trouvées, redirige vers index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
+// server start
 app.listen(port, () => {
   console.log(`Server Started on http://localhost:${port}`);
 });
